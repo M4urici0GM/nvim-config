98c220eef80f889160290414632df2f0e903ccff
Mauricio Barbosa <mgbftw@gmail.com>
2023-08-29 13:38:34 -0300

    configuring java

--- lua/mourice/remap.lua
+++ lua/mourice/remap.lua
@@ -10,13 +10,10 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- Move the line above
 
 vim.keymap.set("n", "<leader>d", "\"_d")     -- Delete without losing buffer in N mode
 vim.keymap.set("v", "<leader>d", "\"_d")     -- Delete without losing buffer in N mode
+vim.keymap.set("n", "<leader>c", "\"_c")     -- Change without losing buffer
+vim.keymap.set("v", "<leader>c", "\"_c")     -- Change without losing buffer
 
 vim.keymap.set("n", "Q", "<nop>")
-
--- Format file
-vim.keymap.set("n", "<leader>f", function()
-    vim.lsp.buf.format()
-end)
 
 -- Nvimtree
 vim.keymap.set("n", "<leader>e", "<cmd> NvimTreeToggle <CR>")
@@ -26,3 +23,16 @@ vim.keymap.set("n", "<leader>E", "<cmd> NvimTreeFocus <CR>")
 -- Terminal
 vim.keymap.set("n", "<C-Esc>", function() nvterminal.toggle("horizontal") end)
 vim.keymap.set("t", "<C-Esc>", function() nvterminal.toggle("horizontal") end)
+
+-- Lsp
+vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format() end)
+vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end)
+vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end)
+vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end)
+vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end)
+vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end)
+vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end)
+vim.keymap.set("n", "<leader>cd", function() vim.diagnostic.open_float() end)
+vim.keymap.set("n", "<leader>cr", function() vim.lsp.buf.references() end)
+vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end)
+vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end)
