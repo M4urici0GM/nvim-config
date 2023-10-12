891abc32ab0e42a7e73b2b4eee126ea99a05d1e4
Mauricio Barbosa <mauricio.barbosa@mercadolivre.com>
2023-10-11 21:09:58 -0300

    fixed some bugs

--- init.lua
+++ init.lua
@@ -11,7 +11,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-
 require("lazy").setup({
     "nvim-tree/nvim-tree.lua",
     { "nvim-tree/nvim-web-devicons", lazy = true },
--- lua/mourice/terminal.lua
+++ lua/mourice/terminal.lua
@@ -5,7 +5,7 @@ local function toggle_horizontal()
     terminal.toggle "horizontal"
 end
 
-vim.keymap.set("n", "<M-Esc>", toggle_horizontal)
-vim.keymap.set("t", "<M-Esc>", toggle_horizontal)
+vim.keymap.set("n", "<C-T>", toggle_horizontal)
+vim.keymap.set("t", "<C-T>", toggle_horizontal)
 
 nvterm.setup({ })
