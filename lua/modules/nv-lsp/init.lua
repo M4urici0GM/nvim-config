a053aa99b63df04efcd80076884784e4dcbbb590
Mauricio Barbosa <37017091+M4urici0GM@users.noreply.github.com>
2024-10-13 22:12:38 -0300

    temp

--- lua/modules/nv-lsp/init.lua
+++ lua/modules/nv-lsp/init.lua
@@ -12,16 +12,14 @@ local servers = {
 			omnisharp.setup({
 				capabilities = capabilities,
 				enable_editorconfig_support = true,
-				enable_ms_build_load_projects_on_demand = false,
-				enable_roslyn_analyzers = false,
+				enable_ms_build_load_projects_on_demand = true,
+				enable_roslyn_analyzers = true,
 				organize_imports_on_format = true,
 				enable_import_completion = true,
 				sdk_include_prereleases = true,
 				analyze_open_documents_only = false,
 				handlers = {
 					["textDocument/definition"] = require('omnisharp_extended').definition_handler,
-					["textDocument/typeDefinition"] = require('omnisharp_extended').type_definition_handler,
-					["textDocument/references"] = require('omnisharp_extended').references_handler,
 					["textDocument/implementation"] = require('omnisharp_extended').implementation_handler,
 				}
 			})
--- plugin/colors.lua
+++ plugin/colors.lua
@@ -1,4 +1,4 @@
-vim.cmd("colorscheme moonfly")
+vim.cmd("colorscheme catppuccin-mocha")
 
 vim.api.nvim_set_hl(0, 'DapBreakpoint', { ctermbg = 'red', bg = '#3A2323', blend = 1, })
 vim.api.nvim_set_hl(0, 'DapBreakpointSymbol', { fg = "#cc6460", bg = '#3A2323', blend = 1, })
